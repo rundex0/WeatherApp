72dbff618c8182f72b3336b851d94ba6e183f166
Nathan <nathan.carney@knights.ucf.edu>
2023-09-10 21:12:12 -0400

    added days of the week

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const axios = require('axios');
-require('dotenv').config(); 
+// const express = require('express');
+// const axios = require('axios');
+// require('dotenv').config(); 
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-const WEATHER_API_KEY = process.env."KEY";
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+// const WEATHER_API_KEY = process.env."KEY";
 
-app.use(express.json());
+// app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log('Server is running on $(PORT)');
-});
+// app.listen(PORT, () => {
+//     console.log('Server is running on $(PORT)');
+// });
