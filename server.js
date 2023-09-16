0a0dc2c9d2ffbf2e2c99a5e1f8b6e4e49dc89eea
rundex0 <69157728+rundex0@users.noreply.github.com>
2023-09-16 17:04:10 -0400

    added get current coordinates feature

--- server.js
+++ server.js
@@ -1,5 +1,35 @@
-const app = {
-    init: () => {
-        
-    }
-};
+const getCurrLoc = document.getElementById("get-location");
+const latEl = document.getElementById("lat");
+const lonEl = document.getElementById("lon");
+
+let location_timeout;
+
+function geolocFail() {
+    alert("Enable GeoLocation Services");
+    clearTimeout(location_timeout); // Clear the timeout if geolocation fails
+}
+
+getCurrLoc.addEventListener("click", () => {
+  if (navigator.geolocation) {
+    location_timeout = setTimeout(geolocFail, 10000);
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        clearTimeout(location_timeout); // Clear the timeout when geolocation succeeds
+        console.log("button clicked");
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+
+        latEl.value = lat.toFixed(2);
+        lonEl.value = long.toFixed(2);
+        console.log(lat + ": latitude, " + long + ": longitude");
+      },
+      function (error) {
+        clearTimeout(location_timeout); // Clear the timeout if there's an error
+        geolocFail();
+      }
+    );
+  }
+  else {
+    geolocFail();
+  }
+});
